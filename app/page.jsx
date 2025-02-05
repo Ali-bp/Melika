6b34f5e52b170c463968378bbe27d3b58fdf16e1
Ali Bakhshalipour <alibakhshalipour@Alis-MacBook-Air.local>
2025-02-05 18:19:01 +0000

    first push

---
+++ app/page.jsx
@@ -0,0 +1,72 @@
+import '@/styles/melika.css'
+
+export const metadata = {
+    title: 'Melika Links',
+    description: 'Melika Links'
+}
+
+export default async function Melika() {
+
+    return (
+        <div className='page'>
+            <div className='heading'>
+
+                <img src='/my-img.jpeg' alt='profile' />
+
+
+                <a href='https://www.instagram.com/melxablogdaily?igsh=NHJsZnV5NTFteWM2' className='text'>@melxablogdaily</a>
+
+            </div>
+
+
+
+
+
+            <div className='buttons'>
+
+
+
+                <a href='https://youtube.com/@meliikaaakhalili?si=tkqG3haAtW9Ybl0W' className='bread youtube'>
+                    <img src='https://icons.veryicon.com/png/o/miscellaneous/alibaba-cloud-ui-icon-library/youtube-150.png' alt='youtube-icon' className='icon' />
+                    <button className='button'> YouTube</button>
+                </a>
+
+
+
+
+
+
+                <a href='https://t.me/melxakl' className='bread telegram'>
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png?20220101141644' alt='telegram-icon' className='icon' />
+                    <button className='button'>Telegram</button>
+
+
+
+
+
+                </a>
+
+
+                <a href='https://www.instagram.com/melxablogdaily?igsh=NHJsZnV5NTFteWM2' className='bread instagram'>
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1200px-Instagram_icon.png' alt='instagram-icon' className='icon' />
+                    <button className='button'>Instagram</button>
+
+
+
+
+
+
+
+
+
+                </a>
+
+
+
+
+            </div>
+        </div >
+
+    )
+
+}
